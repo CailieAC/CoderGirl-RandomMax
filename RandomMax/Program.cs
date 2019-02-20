dc58563912cb553dc9bf05fda0c5f8b84f6ae9d8
Cailie Carlile <cailiecarlile@gmail.com>
2019-02-19 21:45:32 -0600

    commit1

--- RandomMax/Program.cs
+++ RandomMax/Program.cs
@@ -7,7 +7,19 @@ namespace RandomMax
         public static void Main()
         {
             // TODO: Generate a list of 100 random integers between 0 and 1000 and ouput the maximum value.
+            int[] numList = new int[100];
 
+            Random rnd = new Random();
+            int max = 0;
+            for (int count = 0; count < 100; count++)
+            {
+                numList[count] = rnd.Next(0,1000);
+                //Console.WriteLine(numList[count]);
+                if (numList[count] > max)
+                    max = numList[count];
+            }
+            
+            Console.WriteLine(max);
             Console.ReadLine();
         }
     }
